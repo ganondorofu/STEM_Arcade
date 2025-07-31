7b1d44447b907f9562091e0da0c3145588b89142
ganondorofu <ganondorofu123@gmail.com>
2025-07-31 02:06:28 +0000

    192.168.0.95 - - [31/Jul/2025 11:03:58] "GET /games/game_1753926736256_d

--- backend.py
+++ backend.py
@@ -83,18 +83,22 @@ def reupload_game():
         return jsonify({"error": "id は必須です"}), 400
 
     target_dir = os.path.join(GAMES_DIR, game_id)
-    if not os.path.isdir(target_dir):
-        # 既存のディレクトリがない場合は新規作成する
-        os.makedirs(target_dir, exist_ok=True)
+    # 既存のディレクトリがない場合は新規作成する
+    os.makedirs(target_dir, exist_ok=True)
 
     # zip再展開
     if zip_file and zip_file.filename != '':
         try:
             # 再アップロードの前に、一度ディレクトリをクリーンアップする
-            if os.path.isdir(target_dir):
-                shutil.rmtree(target_dir)
-            os.makedirs(target_dir, exist_ok=True)
-            
+            # ただし、img.pngは消さないようにする
+            for item in os.listdir(target_dir):
+                if item != 'img.png':
+                    item_path = os.path.join(target_dir, item)
+                    if os.path.isdir(item_path):
+                        shutil.rmtree(item_path)
+                    else:
+                        os.remove(item_path)
+
             zip_path = os.path.join(target_dir, 'upload.zip')
             zip_file.save(zip_path)
             with zipfile.ZipFile(zip_path, 'r') as zip_ref:
@@ -158,3 +162,4 @@ if __name__ == '__main__':
     CORS(app) 
     app.run(host='0.0.0.0', port=5000)
 
+    
